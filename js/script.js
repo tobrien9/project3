6a7fb9a845e660d9c3b3db109928bdd41eff5df7
tobrien9 <143633513+tobrien9@users.noreply.github.com>
2023-11-28 10:38:25 -0600

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: { lat: 41.69603538026582, lng: -87.70554806440326 },
+    zoom: 15,
+  });
 
+  var marker = new google.maps.Marker({
+    position: { lat: 41.69603538026582, -87.70554806440326 },
+    map: map,
+  });
+
+  var infowindow = new google.maps.InfoWindow({
+    content: "My school",
+  });
+  infowindow.open(map, marker);
+
+    var features = [
+    { position: { lat: 41.69603538026582, lng: -87.70554806440326 }, type: "Mt. Greenwood" },
+  ];
+  
+for (let i = 0; i < features.length; i++) {
+    const marker = new google.maps.Marker({
+      position: features[i].position,
+      icon: icons[features[i].type].icon,
+      map: map,
+    });
+  }
+}
+
+google.maps.event.addDomListener(window, 'load', initMap);
